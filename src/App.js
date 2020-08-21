7f45220313db80345e72decd1fa126a776a036f3
Karesz <torok.karoly.krisztian@gmail.com>
2020-08-21 20:05:19 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Title from './components/Title';
+import UploadForm from './components/UploadForm';
+import ImageGrid from './components/ImageGrid';
+import Modal from './components/Modal';
+
+function App() {
+  const [selectedImage, setSelectedImage] = useState();
+  return (
+    <div className="App">
+      <Title />
+      <UploadForm />
+      <ImageGrid setSelectedImage={setSelectedImage} />
+      { selectedImage && <Modal selectedImage={selectedImage } setSelectedImage={setSelectedImage} /> }
+    </div>
+  );
+}
+
+export default App;
